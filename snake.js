fbe3e595398c31c7736c6d9eb0816d7091825af5
kelverssg <56243708+kelverssg@users.noreply.github.com>
2020-11-03 22:39:54 +0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -27,7 +27,7 @@ function gameLoop() {
     
     requestAnimationFrame(gameLoop);
     if (++count < 4) return;
-    if (paused) cancelAnimationFrame(gameLoop);
+    if (paused) throwError();
 
     count = 0;
     context.clearRect(0, 0, canvas.width, canvas.height);
